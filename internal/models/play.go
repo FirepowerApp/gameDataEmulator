7b0dc943a8326c8c49508327dd261029c3ba8c87
NelsonBlakeN <mail@blakenelson.me>
2026-03-23 08:22:53 -0500

    Add NHL time fields to play-by-play model and test data (#5)

--- internal/models/play.go
+++ internal/models/play.go
@@ -1,9 +1,18 @@
 package models
 
+// PeriodDescriptor holds information about the period of a play
+type PeriodDescriptor struct {
+	Number               int    `json:"number"`
+	PeriodType           string `json:"periodType"`
+	MaxRegulationPeriods int    `json:"maxRegulationPeriods"`
+}
+
 // Play represents a single play in a hockey game
 type Play struct {
-	TypeDescKey string `json:"typeDescKey"`
-	Time        int    `json:"time"`
+	TypeDescKey      string           `json:"typeDescKey"`
+	PeriodDescriptor PeriodDescriptor `json:"periodDescriptor"`
+	TimeInPeriod     string           `json:"timeInPeriod"`
+	TimeRemaining    string           `json:"timeRemaining"`
 }
 
 // PlayByPlayResponse represents the structure of the NHL play-by-play API response
